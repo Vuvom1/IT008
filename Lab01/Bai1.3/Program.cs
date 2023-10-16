147c583800bf96e9ea070a25ca830348f93cdac5
Vuvom1 <146900726+Vuvom1@users.noreply.github.com>
2023-10-16 20:33:43 +0700

    Add Bai1.3+1.5

---
+++ Lab01/Bai1.3/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Bai1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 5, 3 };
+            Console.WriteLine(MaxInt(number));
+        }
+        static int MaxInt(int[] numbers)
+        {
+            int max = numbers[0];
+            foreach (int i in numbers)
+            {
+                if (max < i) max = i;
+            }
+            return max;
+        }
+        static double MaxDou(double[] numbers)
+        {
+            double max = numbers[0];
+            foreach (double i in numbers)
+            {
+                if (max < i) max = i;
+            }
+            return max;
+        }
+        static string MaxString(string[] line)
+        {
+            string res = line[0];
+            foreach (string s in line)
+            {
+                if (s.Length > res.Length) res = s;
+            }
+            return res;
+        }
+        static int MinInt(int[] numbers)
+        {
+            int min = numbers[0];
+            foreach (int i in numbers)
+            {
+                if (min > i) min = i;
+            }
+            return min;
+        }
+        static double MinDou(double[] numbers)
+        {
+            double min = numbers[0];
+            foreach (double i in numbers)
+            {
+                if (min > i) min = i;
+            }
+            return min;
+        }
+        static string MinString(string[] line)
+        {
+            string res = line[0];
+            foreach (string s in line)
+            {
+                if (s.Length < res.Length) res = s;
+            }
+            return res;
+        }
+    }
+}
---
+++ Lab01/Bai1.5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Bai1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = args[0];
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+
+            if (!di.Exists)
+            {
+                Console.WriteLine("Folder does not exist");
+                return;
+            }
+
+            FileInfo[] fileArr = di.GetFiles();
+
+            foreach (FileInfo file in fileArr)
+                Console.WriteLine(file.Name);
+        }
+    }
+}
