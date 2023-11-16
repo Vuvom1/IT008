35985a6244064eaf85fd7be4605ad04b39e2a04b
22520187 <146148909+22520187@users.noreply.github.com>
2023-11-16 13:23:24 +0700

    Add files via upload

---
+++ Lap2_BaiTap_2.4/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap_2._4
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Vẽ chuỗi "Paint Event" tại một vị trí x, y ngẫu nhiên
+            int x = random.Next(this.Width);
+            int y = random.Next(this.Height);
+            g.DrawString("Paint Event", new Font("Arial", 20), Brushes.Black, x, y);
+        }
+    }
+}
